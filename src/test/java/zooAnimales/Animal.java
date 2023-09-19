e6809893150a4d06578d7e4010e7386ba5cfa2bd
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-09-19 17:53:41 -0500

    quinto intento

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -56,8 +56,8 @@ public class Animal {
 	}
 	
 	public static String totalPorTipo() {
-		return "Mamiferos: " + Mamifero.cantidadMamiferos()+"\n"+"Aves: "+Ave.cantidadAves()+"\n"+"Reptiles: "+Reptil.cantidadReptiles()+
-				"\n"+"Peces: "+Pez.cantidadPeces()+"\n"+"Anfibios: "+Anfibio.cantidadAnfibios();
+		return "Mamiferos:" + Mamifero.cantidadMamiferos()+"\n"+"Aves:"+Ave.cantidadAves()+"\n"+"Reptiles:"+Reptil.cantidadReptiles()+
+				"\n"+"Peces:"+Pez.cantidadPeces()+"\n"+"Anfibios:"+Anfibio.cantidadAnfibios();
 	}
 
 	public String toString() {
